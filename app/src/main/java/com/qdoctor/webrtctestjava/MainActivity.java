62ba8d923bae0a54ab4e57f90f3178d0d94456b9
Janos Vajda <vajdajanos@gmail.com>
2019-02-23 13:47:11 +0000

    First commit

---
+++ app/src/main/java/com/qdoctor/webrtctestjava/MainActivity.java
@@ -0,0 +1,204 @@
+package com.qdoctor.webrtctestjava;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 101;
+
+    private WebView mWebView;
+    private String mCustomURL = "https://fiddle.jshell.net/t25gzfeh/show";
+    private String mWebrtcURL = "https://fiddle.jshell.net/t25gzfeh/show";
+    private PermissionRequest myRequest;
+
+    public final static String RESOURCE_VIDEO_CAPTURE = "android.webkit.resource.VIDEO_CAPTURE";
+    public final static String RESOURCE_AUDIO_CAPTURE = "android.webkit.resource.AUDIO_CAPTURE";
+    public final static String RESOURCE_PROTECTED_MEDIA_ID =
+            "android.webkit.resource.PROTECTED_MEDIA_ID";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkForAndAskForPermissions();
+
+    }
+
+    private void createWebView() {
+
+        mWebView = (WebView) findViewById(R.id.webview);
+        setUpWebViewDefaults(mWebView);
+        mWebView.loadUrl(mCustomURL);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+
+            public boolean onConsoleMessage(ConsoleMessage m) {
+                Log.d("getUserMedia, WebView", m.message() + " -- From line "
+                        + m.lineNumber() + " of "
+                        + m.sourceId());
+
+                return true;
+            }
+
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                myRequest = request;
+
+                for (String permission : request.getResources()) {
+                    switch (permission) {
+                        case "android.webkit.resource.AUDIO_CAPTURE": {
+                            askForPermission(request.getOrigin().toString(), Manifest.permission.RECORD_AUDIO, MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+        Toast.makeText(this, "LOAD1", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void askForPermission(String origin, String permission, int requestCode) {
+        Log.d("WebView", "inside askForPermission for" + origin + "with" + permission);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                permission)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    permission)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{permission},
+                        requestCode);
+            }
+        } else {
+            myRequest.grant(myRequest.getResources());
+        }
+    }
+
+    private void checkForAndAskForPermissions() {
+
+        String permission = Manifest.permission.CAMERA;
+        String permission2 = Manifest.permission.RECORD_AUDIO;
+        String permission3 = Manifest.permission.MODIFY_AUDIO_SETTINGS;
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED
+
+                ||
+
+                ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.RECORD_AUDIO)
+                        != PackageManager.PERMISSION_GRANTED
+                ||
+
+                ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.MODIFY_AUDIO_SETTINGS)
+                        != PackageManager.PERMISSION_GRANTED
+
+        ) {
+
+            Log.d("getUserMedia, WebView", "FASSZ");
+
+            String[] permission_list = new String[3];
+            permission_list[0] = permission;
+            permission_list[1] = permission2;
+            permission_list[2] = permission3;
+
+
+            // No explanation needed; request the permission
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.CAMERA)) {
+
+                Log.d("getUserMedia, WebView", "FASSZ");
+
+
+            } else ActivityCompat.requestPermissions(MainActivity.this,
+                    permission_list, 1);
+        } else {
+            // Permission has already been granted
+            Toast.makeText(this, "LOAD2", Toast.LENGTH_SHORT).show();
+            createWebView();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            Toast.makeText(this, "Camera Permission granted", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Camera Permission granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void setUpWebViewDefaults(WebView webView) {
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }
+
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+
+        // Enable remote debugging via chrome://inspect
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        webView.clearCache(true);
+        webView.clearHistory();
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    private void openBrowserWithURL(String url) {
+
+        Uri uri = Uri.parse("googlechrome://navigate?url=" + url);
+        Intent i = new Intent(Intent.ACTION_VIEW, uri);
+        if (i.resolveActivity(MainActivity.this.getPackageManager()) == null) {
+            i.setData(Uri.parse(url));
+        }
+
+        MainActivity.this.startActivity(i);
+    }
+
+}
